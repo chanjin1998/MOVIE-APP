52ddb6a9fa50295568e9651906dc1bb55c07da69
chanjin1998 <chanjin@kookmin.ac.kr>
2023-11-30 16:35:47 +0900

    feat: movielist 배열 데이터 갱신

--- src/store/movie.js
+++ src/store/movie.js
@@ -8,11 +8,16 @@ const store = new Store({
 
 export default store;
 export const searchMovies = async (page) => {
+  if (page === 1) {
+    store.state.page = 1;
+    store.state.movies = [];
+  }
   // 비동기로 동작하니까 await를 사용해서 fetch 함수를 서버로 갔다올 때 기다려야함
   // 동작 순서 : omdb 페이지로 검색 결과를 전송 -> json으로 받아서 콘솔에 출력
   const res = await fetch(
     `https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
   );
-  const json = await res.json();
-  console.log(json);
+  const { Search } = await res.json();
+  // 1페이지의 영화정보와 2페이지의 정보를 Search로 받아온다. 3,4, 계속 받아옴
+  store.state.movies = [...store.state.movies, ...Search];
 };
