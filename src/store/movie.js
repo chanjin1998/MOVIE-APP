fb7aa5b153637ccd87dc2f2b3cf857d70332cde3
chanjin1998 <chanjin@kookmin.ac.kr>
2023-12-20 20:53:01 +0900

    feat: pageMax 추가

--- src/store/movie.js
+++ src/store/movie.js
@@ -3,13 +3,15 @@ import { Store } from "../core/heropy";
 const store = new Store({
   searchText: "",
   page: 1,
+  pageMax : 1,
   movies: [],
 });
 
 export default store;
 export const searchMovies = async (page) => {
+  store.state.page = page //page 값 바뀌는 것 저장하기
   if (page === 1) {
-    store.state.page = 1;
+    // store.state.page = 1;
     store.state.movies = [];
   }
   // 비동기로 동작하니까 await를 사용해서 fetch 함수를 서버로 갔다올 때 기다려야함
@@ -17,7 +19,8 @@ export const searchMovies = async (page) => {
   const res = await fetch(
     `https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
   );
-  const { Search } = await res.json();
+  const { Search,totalResults } = await res.json();
   // 1페이지의 영화정보와 2페이지의 정보를 Search로 받아온다. 3,4, 계속 받아옴
   store.state.movies = [...store.state.movies, ...Search];
+  store.state.pageMax = Math.ceil(Number(totalResults) / 10)
 };
