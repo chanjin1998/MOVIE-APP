1e8fbbe4fe8abaf07f85dd56db77a92f33e288d8
chanjin1998 <chanjin@kookmin.ac.kr>
2023-12-24 16:38:07 +0900

    feat: message 에러 처리

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -11,16 +11,24 @@ export default class MovieList extends Component {
     movieStore.subscribe("loading", () => {
       this.render();
     });
+    movieStore.subscribe("message", () => {
+      this.render();
+    });
   }
   render() {
     this.el.classList.add("movie-list");
     this.el.innerHTML = /*html*/ `
-      <div class='movies'></div>
+      ${
+        movieStore.state.message
+          ? `<div class='message'>${movieStore.state.message}</div>`
+          : `<div class='movies'></div>`
+      }
       <div class="the-loader hide"></div>
     `;
 
     const moviesEl = this.el.querySelector(".movies");
-    moviesEl.append(
+    //옵셔널체이닝 moviesEl값이 존재할때만 추가한다
+    moviesEl?.append(
       //map() 배열데이터에서 사용, 앞의 배열데이터를 기준으로 callback 함수를 반복 실행, 반환 후 재배정
       ...movieStore.state.movies.map((movie) => {
         return new MovieItem({
--- src/store/movie.js
+++ src/store/movie.js
@@ -3,28 +3,39 @@ import { Store } from "../core/heropy";
 const store = new Store({
   searchText: "",
   page: 1,
-  pageMax : 1,
+  pageMax: 1,
   movies: [],
   loading: false,
+  message: "Search for the movie title!",
 });
 
 export default store;
 export const searchMovies = async (page) => {
-  store.state.loading = true
-  store.state.page = page //page 값 바뀌는 것 저장하기
+  store.state.loading = true;
+  store.state.page = page; //page 값 바뀌는 것 저장하기
   if (page === 1) {
     // store.state.page = 1;
     store.state.movies = [];
+    store.state.message = "";
   }
   // 비동기로 동작하니까 await를 사용해서 fetch 함수를 서버로 갔다올 때 기다려야함
   // 동작 순서 : omdb 페이지로 검색 결과를 전송 -> json으로 받아서 콘솔에 출력
-  const res = await fetch(
-    `https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
-  );
-  const { Search,totalResults } = await res.json();
+  try {
+    const res = await fetch(
+      `https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
+    );
+    const { Search, totalResults, Response, Error } = await res.json();
+    if (Response === "True") {
+      store.state.movies = [...store.state.movies, ...Search];
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+    } else {
+      store.state.message = Error;
+    }
+  } catch (error) {
+    console.log("searchMovies error: ", error);
+  } finally {
+    store.state.loading = false;
+    movieList.render();
+  }
   // 1페이지의 영화정보와 2페이지의 정보를 Search로 받아온다. 3,4, 계속 받아옴
-  store.state.movies = [...store.state.movies, ...Search];
-  store.state.pageMax = Math.ceil(Number(totalResults) / 10)
-  store.state.loading = false
-  movieList.render();
 };
