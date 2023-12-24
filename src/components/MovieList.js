9088178ffea5085099c7c2fb76b1c7630fba8e02
chanjin1998 <chanjin@kookmin.ac.kr>
2023-12-24 15:17:25 +0900

    feat: loading 렌더링

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,6 +1,6 @@
 import { Component } from "../core/heropy";
 import movieStore from "../store/movie";
-import MovieItem from './MovieItem'
+import MovieItem from "./MovieItem";
 
 export default class MovieList extends Component {
   constructor() {
@@ -8,11 +8,15 @@ export default class MovieList extends Component {
     movieStore.subscribe("movies", () => {
       this.render();
     });
+    movieStore.subscribe("loading", () => {
+      this.render();
+    });
   }
   render() {
     this.el.classList.add("movie-list");
     this.el.innerHTML = /*html*/ `
       <div class='movies'></div>
+      <div class="the-loader hide"></div>
     `;
 
     const moviesEl = this.el.querySelector(".movies");
@@ -20,9 +24,13 @@ export default class MovieList extends Component {
       //map() 배열데이터에서 사용, 앞의 배열데이터를 기준으로 callback 함수를 반복 실행, 반환 후 재배정
       ...movieStore.state.movies.map((movie) => {
         return new MovieItem({
-          movie: movie
+          movie: movie,
         }).el;
       })
     );
+    const loaderEl = this.el.querySelector(".the-loader");
+    movieStore.state.loading
+      ? loaderEl.classList.remove("hide")
+      : loaderEl.classList.add("hide");
   }
 }
