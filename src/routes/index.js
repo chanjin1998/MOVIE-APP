d6df948e5242a109f2759e0ac8de912a263ff5af
chanjin1998 <chanjin@kookmin.ac.kr>
2024-01-07 19:10:28 +0900

    feat: notfound 페이지 추가(모든 주솟값)

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,12 @@ import { createRouter } from "../core/heropy";
 import Home from "./Home";
 import Movie from "./Movie";
 import About from "./About";
+import NotFound from "./NotFound";
 
 export default createRouter([
   { path: "#/", component: Home },
   { path: "#/movie", component: Movie },
   { path: "#/about", component: About },
+  // 나머지 모든 페이지 에러 처리, 마지막에 적기
+  { path: ".*", component: NotFound },
 ]);
