74d5c8aa19ac8fd72888104cd7f88542752925bc
chanjin1998 <chanjin@kookmin.ac.kr>
2024-01-07 17:43:03 +0900

    feat: about페이지 store 추가

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core/heropy";
+
+export default new Store({
+  photo: "https://heropy.blog/css/images/logo.png",
+  name: "JeongChanJin",
+  email: "jcjin1998@gmail.com",
+  blog: "https://chanjin98.tistory.com/",
+  github: "https://github.com/chanjin1998",
+  repository: "https://github.com/ParkYoungWoong/vanillajs-movie-app",
+});
